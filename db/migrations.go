950e8f5b903830bcc0352ea310453ec58d5237c2
kiarashfz <kiarashfz.fazli@gmail.com>
2025-08-07 20:55:49 +0330

    feat: auto migration from files added

---
+++ db/migrations.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+// RunMigrations executes all pending SQL migration files.
+func RunMigrations(databaseURL string) {
+	m, err := migrate.New("file://db/migrations", databaseURL)
+	if err != nil {
+		log.Fatalf("Could not create migration instance: %v", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Could not apply migrations: %v", err)
+	}
+
+	log.Println("Migrations applied successfully!")
+}
