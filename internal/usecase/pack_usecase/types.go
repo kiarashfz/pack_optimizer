8332fdfad2d36c43667c539c74dc44398e25a176
kiarashfz <kiarashfz.fazli@gmail.com>
2025-08-07 21:01:01 +0330

    feat: type of use case layer

---
+++ internal/usecase/pack_usecase/types.go
@@ -0,0 +1,13 @@
+package pack_usecase
+
+type Pack struct {
+	Size  int `json:"size"`
+	Count int `json:"count"`
+}
+
+type CalculatePacksOutput struct {
+	TotalItems     int    `json:"total_items"`     // Total items that fit in the packs
+	RemainingItems int    `json:"remaining_items"` // Number of empty spaces in packs
+	TotalPacks     int    `json:"total_packs"`     // Total number of packs used
+	Packs          []Pack `json:"packs"`           // Calculated packs with their sizes and counts
+}
