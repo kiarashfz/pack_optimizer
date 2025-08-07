2cd194b6cdaa2408c7cb1c7c820f7e33f7af4eca
kiarashfz <kiarashfz.fazli@gmail.com>
2025-08-07 21:00:23 +0330

    feat: pack handler added using fiber

---
+++ internal/handler/pack_handler/pack_handler.go
@@ -0,0 +1,40 @@
+package pack_handler
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"pack_optimizer/internal/usecase/pack_usecase"
+	"pack_optimizer/pkg/validator"
+)
+
+type PackHandler struct {
+	packUseCase *pack_usecase.PackUseCase
+}
+
+func NewPackHandler(packUseCase *pack_usecase.PackUseCase) *PackHandler {
+	return &PackHandler{packUseCase: packUseCase}
+}
+
+func (h *PackHandler) CalculatePacks(c *fiber.Ctx) error {
+	var req CalculatePacksReq
+
+	// Parse and validate JSON body
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid request",
+		})
+	}
+
+	// Validate the input
+	if err := validator.Validate.Struct(req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	output, err := h.packUseCase.CalculatePacks(c.Context(), req.Quantity)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(output)
+}
